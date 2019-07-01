47a76413adaafbb542f8788d1fd67c3302f13666
santowsa <33058907+santowsa@users.noreply.github.com>
2019-07-01 12:53:31 +0200

    final versions
    sorry no description of parameters

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # Exercise 6
 from pathlib import Path
-
-input_dir  = Path("data")
-
+main_dir = Path ("./")
+output_dir = main_dir / "results"
+import utils as uts
 # 1. Read the Scene that you downloaded from the data directory using SatPy. [2P]
-
+import satpy as satpy
+files = satpy.find_files_and_readers(base_dir="./data", reader="seviri_l1b_nc")
+scn = satpy.Scene(filenames=files) #save it as a satpy scene
+scn.available_composite_names() #to check which composites are aviable
 # 2. Load the composites "natural_color" and "convection" [2P]
-
+scn.load(["natural_color"]) # can be skipped thanks to function printSatImg
+scn.load(["convection"]) # can be skipped thanks to function printSatImg
 # 3. Resample the fulldisk to the Dem. Rep. Kongo and its neighbours [4P] 
 #    by defining your own area in Lambert Azimuthal Equal Area. 
 #    Use the following settings:
@@ -14,5 +18,14 @@ input_dir  = Path("data")
 #      - width and height of the resulting domain: 500px
 #      - projection x/y coordinates of lower left: -15E5
 #      - projection x/y coordinates of upper right: 15E5 
-
+# see function printSatImg in Task 4
 # 4. Save both loaded composites of the resampled Scene as simple png images. [2P]
+uts.dirCreate(output_dir) 
+uts.printSatImg(scn=scn,scene="natural_color",proj="laea",lat_or=-3,lon_or=23,
+                width=500,height=500, llx=-15E5,lly=-15E5,urx=15E5,ury=15E5,
+                saveName="natural_color.png",save_path="results")
+                #please give me feedback (see utils)!
+uts.printSatImg(scn=scn,scene="convection",proj="laea",lat_or=-3,lon_or=23,
+                width=500,height=500, llx=-15E5,lly=-15E5,urx=15E5,ury=15E5,
+                saveName="convection.png",save_path="results")
+                #please give me feedback (see utils)!
--- utils.py
+++ utils.py
@@ -1,4 +1,56 @@
 # utility functions
+def printSatImg(scn, scene, proj, lat_or, lon_or, width, height, llx, lly, 
+                urx, ury, saveName, save_path, 
+                area_id = "NAN", description = "NAN", proj_id = "NAN"):
+    """
+    Creates an Image (.png) of a selected area from a .nc satellite format.
+    Requires satpy loaded!
+    
+    sorry no param & return args maybe in 1.1 
+    
+    Parameters
+    ----------
+    
+    Returns
+    -------
+    
+    """
+    scn.load([scene])
+    from pyresample.geometry import AreaDefinition
+    proj_dict = {"proj": proj, "lat_ts": lat_or, "lon_0": lon_or}
+    area_extent = (llx,lly,urx,ury)
+    area_def = AreaDefinition(area_id, proj_id, description, proj_dict, width, 
+                              height, area_extent)
+    local_scn = scn.resample(area_def)
+    local_scn.save_dataset(dataset_id =scene,
+                           writer="simple_image",
+                           filename=saveName, 
+                           base_dir=save_path)
 
-# Use this file for all functions you create and might want to reuse later.
+#Questions:
+    #a) how can I create a dynamical (like paste0 command in R) 
+        #path for filename and base_dir?
+    #b) do I actually need to classify area_id etc. or can it be blank in the function?
+
+def dirCreate(x):
+    """
+    Creates a subdirectory.
+    
+    Parameters
+    ----------
+    
+    Return
+    ------
+    
+    """
+    subdir = x.exists()
+    if subdir:
+        print("All good! Filepath {} already exists.".format(x))
+    
+    else:
+        x.mkdir(parents = True, exist_ok = True)
+        
+
+
+    # Use this file for all functions you create and might want to reuse later.
 # Import them in the `main.py` script
